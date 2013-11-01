561a2d902f028adc4127496c325dbc607e8a0735
Edward Kmett <ekmett@gmail.com>
2013-11-01 12:06:50 -0400

    Dropped IterF in exchange for Either

--- src/Control/Monad/Trans/Iter.hs
+++ src/Control/Monad/Trans/Iter.hs
@@ -27,7 +27,6 @@
 ----------------------------------------------------------------------------
 module Control.Monad.Trans.Iter
   ( MonadFree(..)
-  , IterF(..)
   , IterT(..)
   , delay
   , retract
@@ -42,13 +41,11 @@ import Control.Monad.Trans.Class
 import Control.Monad.Free.Class
 import Control.Monad.State.Class
 import Control.Monad.Reader.Class
-import Data.Bifoldable
 import Data.Bifunctor
 import Data.Bitraversable
 import Data.Functor.Bind
 import Data.Functor.Identity
 import Data.Foldable
-import Data.Monoid
 import Data.Traversable
 import Data.Semigroup.Foldable
 import Data.Semigroup.Traversable
@@ -58,59 +55,27 @@ import Data.Typeable
 import Data.Data
 #endif
 
-data IterF a b = Pure a | Iter b
-  deriving (Eq,Ord,Show,Read,Typeable)
-
-instance Functor (IterF a) where
-  fmap _ (Pure a) = Pure a
-  fmap f (Iter b) = Iter (f b)
-
-instance Foldable (IterF a) where
-  foldMap f (Iter b) = f b
-  foldMap _ _         = mempty
-
-instance Traversable (IterF a) where
-  traverse _ (Pure a) = pure (Pure a)
-  traverse f (Iter b) = Iter <$> f b
-
-instance Bifunctor IterF where
-  bimap f _ (Pure a) = Pure (f a)
-  bimap _ g (Iter b) = Iter (g b)
-
-instance Bifoldable IterF where
-  bifoldMap f _ (Pure a) = f a
-  bifoldMap _ g (Iter b) = g b
-
-instance Bitraversable IterF where
-  bitraverse f _ (Pure a) = Pure <$> f a
-  bitraverse _ g (Iter b) = Iter <$> g b
-
-iterF :: (a -> r) -> (b -> r) -> IterF a b -> r
-iterF f _ (Pure a) = f a
-iterF _ g (Iter b) = g b
-{-# INLINE iterF #-}
-
 -- | The monad supporting iteration based over a base monad @m@.
 --
 -- @
 -- 'IterT' ~ 'FreeT' 'Identity'
 -- @
-data IterT m a = IterT { runIterT :: m (IterF a (IterT m a)) }
+data IterT m a = IterT { runIterT :: m (Either a (IterT m a)) }
 #if __GLASGOW_HASKELL__ >= 707
   deriving (Typeable)
 #endif
 
-instance Eq (m (IterF a (IterT m a))) => Eq (IterT m a) where
+instance Eq (m (Either a (IterT m a))) => Eq (IterT m a) where
   IterT m == IterT n = m == n
 
-instance Ord (m (IterF a (IterT m a))) => Ord (IterT m a) where
+instance Ord (m (Either a (IterT m a))) => Ord (IterT m a) where
   compare (IterT m) (IterT n) = compare m n
 
-instance Show (m (IterF a (IterT m a))) => Show (IterT m a) where
+instance Show (m (Either a (IterT m a))) => Show (IterT m a) where
   showsPrec d (IterT m) = showParen (d > 10) $
     showString "IterT " . showsPrec 11 m
 
-instance Read (m (IterF a (IterT m a))) => Read (IterT m a) where
+instance Read (m (Either a (IterT m a))) => Read (IterT m a) where
   readsPrec d =  readParen (d > 10) $ \r ->
     [ (IterT m,t) | ("IterT",s) <- lex r, (m,t) <- readsPrec 11 s]
 
@@ -119,15 +84,15 @@ instance Monad m => Functor (IterT m) where
   {-# INLINE fmap #-}
 
 instance Monad m => Applicative (IterT m) where
-  pure = IterT . return . Pure
+  pure = IterT . return . Left
   {-# INLINE pure #-}
   (<*>) = ap
   {-# INLINE (<*>) #-}
 
 instance Monad m => Monad (IterT m) where
-  return = IterT . return . Pure
+  return = IterT . return . Left
   {-# INLINE return #-}
-  IterT m >>= k = IterT $ m >>= iterF (runIterT . k) (return . Iter . (>>= k))
+  IterT m >>= k = IterT $ m >>= either (runIterT . k) (return . Right . (>>= k))
   {-# INLINE (>>=) #-}
   fail = IterT . fail
   {-# INLINE fail #-}
@@ -141,9 +106,7 @@ instance Monad m => Bind (IterT m) where
   {-# INLINE (>>-) #-}
 
 instance MonadFix m => MonadFix (IterT m) where
-  mfix f = IterT $ mfix (runIterT . f . unPure) where
-    unPure (Pure x)  = x
-    unPure (Iter _) = error "mfix (IterT m): Iter"
+  mfix f = IterT $ mfix $ runIterT . f . either id (error "mfix (IterT m): Right")
   {-# INLINE mfix #-}
 
 instance MonadPlus m => Alternative (IterT m) where
@@ -160,15 +123,15 @@ instance MonadPlus m => MonadPlus (IterT m) where
 
 -- | This is not a true monad transformer. It is only a monad transformer \"up to 'retract'\".
 instance MonadTrans IterT where
-  lift = IterT . liftM Pure
+  lift = IterT . liftM Left
   {-# INLINE lift #-}
 
 instance Foldable m => Foldable (IterT m) where
-  foldMap f = foldMap (iterF f (foldMap f)) . runIterT
+  foldMap f = foldMap (either f (foldMap f)) . runIterT
   {-# INLINE foldMap #-}
 
 instance Foldable1 m => Foldable1 (IterT m) where
-  foldMap1 f = foldMap1 (iterF f (foldMap1 f)) . runIterT
+  foldMap1 f = foldMap1 (either f (foldMap1 f)) . runIterT
   {-# INLINE foldMap1 #-}
 
 instance (Monad m, Traversable m) => Traversable (IterT m) where
@@ -177,19 +140,9 @@ instance (Monad m, Traversable m) => Traversable (IterT m) where
 
 instance (Monad m, Traversable1 m) => Traversable1 (IterT m) where
   traverse1 f (IterT m) = IterT <$> traverse1 go m where
-    go (Pure a) = Pure <$> f a
-    go (Iter a) = Iter <$> traverse1 f a
+    go (Left a) = Left <$> f a
+    go (Right a) = Right <$> traverse1 f a
   {-# INLINE traverse1 #-}
-
-{-
-instance MonadWriter e m => MonadWriter e (IterT m) where
-  tell = lift . tell
-  {-# INLINE tell #-}
-  listen = lift . listen . retract
-  {-# INLINE listen #-}
-  pass = lift . pass . retract
-  {-# INLINE pass #-}
--}
 
 instance (Functor m, MonadReader e m) => MonadReader e (IterT m) where
   ask = lift ask
@@ -207,20 +160,8 @@ instance (Functor m, MonadState s m) => MonadState s (IterT m) where
   {-# INLINE state #-}
 #endif
 
-{-
-instance (Functor m, MonadError e m) => MonadError e (Free m) where
-  throwError = lift . throwError
-  {-# INLINE throwError #-}
-  catchError as f = lift (catchError (retract as) (retract . f))
-  {-# INLINE catchError #-}
-
-instance (Functor m, MonadCont m) => MonadCont (Free m) where
-  callCC f = lift (callCC (retract . f . liftM lift))
-  {-# INLINE callCC #-}
--}
-
 instance Monad m => MonadFree Identity (IterT m) where
-  wrap = IterT . return . Iter . runIdentity
+  wrap = IterT . return . Right . runIdentity
   {-# INLINE wrap #-}
 
 delay :: (Monad f, MonadFree f m) => m a -> m a
@@ -234,11 +175,11 @@ delay = wrap . return
 -- 'retract' . 'lift' = 'id'
 -- @
 retract :: Monad m => IterT m a -> m a
-retract m = runIterT m >>= iterF return retract
+retract m = runIterT m >>= either return retract
 
 -- | Tear down a 'Free' 'Monad' using iteration.
 iter :: Monad m => (m a -> a) -> IterT m a -> a
-iter phi (IterT m) = phi (iterF id (iter phi) `liftM` m)
+iter phi (IterT m) = phi (either id (iter phi) `liftM` m)
 
 -- | Lift a monad homomorphism from @m@ to @n@ into a Monad homomorphism from @'IterT' m@ to @'IterT' n@.
 hoistIterT :: Monad n => (forall a. m a -> n a) -> IterT m b -> IterT n b
@@ -260,7 +201,7 @@ freeTyCon = mkTyCon3 "free" "Control.Monad.Iter" "IterT"
 
 instance
   ( Typeable1 m, Typeable a
-  , Data (m (IterF a (IterT m a)))
+  , Data (m (Either a (IterT m a)))
   , Data a
   ) => Data (IterT m a) where
     gfoldl f z (IterT as) = z IterT `f` as
