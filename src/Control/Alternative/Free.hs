34bdce9fc8a8a333c76d7be15b03970470144846
Víctor López Juan <victor@lopezjuan.com>
2014-03-18 20:36:21 +0100

    Sketch of left-associative instance for alternative, with tests

--- src/Control/Alternative/Free.hs
+++ src/Control/Alternative/Free.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE CPP #-}
 {-# LANGUAGE Rank2Types #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 #if __GLASGOW_HASKELL__ >= 707
 {-# LANGUAGE DeriveDataTypeable #-}
 #endif
@@ -20,9 +21,10 @@
 ----------------------------------------------------------------------------
 module Control.Alternative.Free
   ( Alt(..)
-  , runAlt
-  , liftAlt
-  , hoistAlt
+    , AltF(..)
+    , runAlt
+    , liftAlt
+    , hoistAlt
   ) where
 
 import Control.Applicative
@@ -33,73 +35,101 @@ import Data.Semigroup
 import Data.Typeable
 #endif
 
--- | The free 'Alternative' for a 'Functor' @f@.
-data Alt f a where
-  Pure :: a -> Alt f a
-  Ap   :: f a -> Alt f (a -> b) -> Alt f b
-  Alt  :: [Alt f a] -> Alt f a
+data AltF f a where
+  Ap     :: f (a -> b) -> Alt f a -> AltF f b
+  Pure   :: a                     -> AltF f  a
 #if __GLASGOW_HASKELL__ >= 707
-  deriving (Typeable)
+  deriving Typeable
 #endif
 
--- | Given a natural transformation from @f@ to @g@, this gives a canonical monoidal natural transformation from @'Alt' f@ to @g@.
-runAlt :: Alternative g => (forall x. f x -> g x) -> Alt f a -> g a
-runAlt _ (Pure x) = pure x
-runAlt u (Ap f x) = flip id <$> u f <*> runAlt u x
-runAlt u (Alt as) = foldr (\a r -> runAlt u a <|> r) empty as
+newtype Alt f a = Alt { alternatives :: [AltF f a] }
+#if __GLASGOW_HASKELL__ >= 707
+  deriving Typeable
+#endif
 
-instance Functor (Alt f) where
-  fmap f (Pure a)   = Pure (f a)
-  fmap f (Ap x y)   = Ap x ((f .) <$> y)
-  fmap f (Alt as)   = Alt (fmap f <$> as)
+instance Functor f => Functor (AltF f) where
+  fmap f (Pure a) = Pure (f a)
+  fmap f (Ap g x) = Ap (fmap (f .) g) x
 
-instance Apply (Alt f) where
-  Pure f <.> y = fmap f y
-  Ap x y <.> z = Ap x (flip <$> y <.> z)
-  Alt as <.> z = Alt (map (<.> z) as) -- This assumes 'left distribution'
+instance Functor f => Functor (Alt f) where
+  fmap f (Alt xs) = Alt $ map (fmap f) xs
+  
+infixl 3 `Ap`
 
-instance Applicative (Alt f) where
+instance Functor f => Applicative (AltF f) where
   pure = Pure
-  Pure f <*> y = fmap f y
-  Ap x y <*> z = Ap x (flip <$> y <*> z)
-  Alt as <*> z = Alt (map (<*> z) as) -- This assumes 'left distribution'
+  (Pure f)   <*> y                = fmap f y      -- fmap
+  y          <*> (Pure a)         = fmap ($ a) y  -- interchange
+  (Ap f a)   <*> (Ap g b)         = -- (f <*> a) <*> (g <*> b) == (fmap mangle f) <*> ((,) <$> a <*> ((,) <*> w <*> t))
+                                    (fmap mangle f `Ap` ((fmap (,) a) <*> (Alt [Ap (fmap (,) g) b])))
+                                      where
+                                      mangle :: (a -> b -> c) -> (a, (d -> b, d)) -> c
+                                      mangle f' (a',(g',d)) = f' a' (g' d)
+ 
+instance Functor f => Applicative (Alt f) where
+  pure a = Alt [pure a]
 
-instance Alternative (Alt f) where
+  -- distributivity of alternative
+  (Alt xs) <*> ys = Alt (xs >>= alternatives . (`ap'` ys))
+    where
+
+      -- Pure f <*> u == fmap f u
+      ap' :: (Functor f) => AltF f (a -> b) -> Alt f a -> Alt f b
+      Pure f `ap'` u@(Alt _)  = fmap f u
+
+      -- (u <*> v <*> w) == uncurry <$> u <*> ((,) <$> v <*> w)
+      (Ap u v) `ap'` w  = Alt [(uncurry `fmap` u) `Ap` (((,) <$> v) <*> w)]
+  
+
+liftAltF :: (Functor f) => f a -> AltF f a
+liftAltF x = Ap (fmap const x) (Alt [Pure ()])
+
+-- | A version of 'lift' that can be used with just a 'Functor' for @f@.
+liftAlt :: (Functor f) => f a -> Alt f a
+liftAlt = Alt . (:[]) . liftAltF
+    
+-- | Given a natural transformation from @f@ to @g@, this gives a canonical monoidal natural transformation from @'Alt' f@ to @g@.
+runAlt :: forall f g a. Alternative g => (forall x. f x -> g x) -> Alt f a -> g a
+runAlt u xs0 = go xs0 where
+  
+  go  :: Alt f b -> g b
+  go (Alt xs) = foldr (\r a -> (go2 r) <|> a) empty xs
+
+  go2 :: AltF f b -> g b
+  go2 (Pure a) = pure a
+  go2 (Ap f x) = u f <*> go x
+
+
+instance (Functor f) => Apply (Alt f) where
+  (<.>) = (<*>)
+                     
+instance (Functor f) => Alternative (Alt f) where
   empty = Alt []
   {-# INLINE empty #-}
-  Alt [] <|> r      = r
-  l      <|> Alt [] = l
   Alt as <|> Alt bs = Alt (as ++ bs)
-  l      <|> r      = Alt [l, r]
   {-# INLINE (<|>) #-}
 
-instance Semigroup (Alt f a) where
+instance (Functor f) => Semigroup (Alt f a) where
   (<>) = (<|>)
   {-# INLINE (<>) #-}
 
-instance Monoid (Alt f a) where
+instance (Functor f) => Monoid (Alt f a) where
   mempty = empty
   {-# INLINE mempty #-}
   mappend = (<|>)
   {-# INLINE mappend #-}
-  mconcat as = fromList (as >>= toList)
-    where
-      toList (Alt xs) = xs
-      toList x       = [x]
-      fromList [x] = x
-      fromList xs  = Alt xs
+  mconcat as = Alt (as >>= alternatives)
   {-# INLINE mconcat #-}
 
--- | A version of 'lift' that can be used with just a 'Functor' for @f@.
-liftAlt :: f a -> Alt f a
-liftAlt x = Ap x (Pure id)
 {-# INLINE liftAlt #-}
+
+hoistAltF :: (forall a. f a -> g a) -> AltF f b -> AltF g b
+hoistAltF _ (Pure a) = Pure a
+hoistAltF f (Ap x y) = Ap (f x) (hoistAlt f y)
 
 -- | Given a natural transformation from @f@ to @g@ this gives a monoidal natural transformation from @Alt f@ to @Alt g@.
 hoistAlt :: (forall a. f a -> g a) -> Alt f b -> Alt g b
-hoistAlt _ (Pure a) = Pure a
-hoistAlt f (Ap x y) = Ap (f x) (hoistAlt f y)
-hoistAlt f (Alt as) = Alt (map (hoistAlt f) as)
+hoistAlt f (Alt as) = Alt (map (hoistAltF f) as)
 
 #if defined(GHC_TYPEABLE) && __GLASGOW_HASKELL__ < 707
 instance Typeable1 f => Typeable1 (Alt f) where
@@ -116,3 +146,4 @@ altTyCon = mkTyCon3 "free" "Control.Alternative.Free" "Alt"
 {-# NOINLINE altTyCon #-}
 
 #endif
+
