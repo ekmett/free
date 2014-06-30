daa8aa3c073abb1491d4da89073004682cb4d90a
Edward Kmett <ekmett@gmail.com>
2014-06-29 22:00:21 -0400

    added Catenated

---
+++ Catenated.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE RankNTypes, PolyKinds #-}
+module Catenated where
+
+import Control.Applicative
+import Control.Category
+import Data.Functor.Identity
+import Prelude hiding ((.),id)
+
+class Catenated t where
+  foldCat     :: Category s => (forall a b. r a b -> s a b) -> t r a b -> s a b
+  traverseCat :: Applicative m => (forall a b. r a b -> m (s a b)) -> t r a b -> m (t s a b)
+
+mapCat :: Catenated t => (forall a b. r a b -> s a b) -> t r a b -> t s a b
+mapCat f = runIdentity . traverseCat (Identity . f)
