eece8edbeeec77d17ab65958a75763fdef185273
Vaibhav Sagar <vaibhavsagar@gmail.com>
2018-03-05 16:27:19 +0800

    src/Control/Monad/Free/Ap.hs: escape < and > in docs

--- src/Control/Monad/Free/Ap.hs
+++ src/Control/Monad/Free/Ap.hs
@@ -12,17 +12,17 @@
 -- |
 -- \"Applicative Effects in Free Monads\"
 --
--- Often times, the '(<*>)' operator can be more efficient than 'ap'.
+-- Often times, the '(\<*\>)' operator can be more efficient than 'ap'.
 -- Conventional free monads don't provide any means of modeling this.
 -- The free monad can be modified to make use of an underlying applicative.
--- But it does require some laws, or else the '(<*>)' = 'ap' law is broken.
+-- But it does require some laws, or else the '(\<*\>)' = 'ap' law is broken.
 -- When interpreting this free monad with 'foldFree',
 -- the natural transformation must be an applicative homomorphism.
 -- An applicative homomorphism @hm :: (Applicative f, Applicative g) => f x -> g x@
 -- will satisfy these laws.
 --
 -- * @hm (pure a) = pure a@
--- * @hm (f <*> a) = hm f <*> hm a@
+-- * @hm (f \<*\> a) = hm f \<*\> hm a@
 --
 -- This is based on the \"Applicative Effects in Free Monads\" series of articles by Will Fancher
 --
